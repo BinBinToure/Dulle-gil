7891888fcab976b7fa402de62b60b20018dbda3a
BinBinToure <ijbin4327@naver.com>
2022-11-15 13:21:12 +0900

    2022.11.15 main + server

---
+++ seoulgil/src/main/java/com/introduce/seoulgil/SeoulgilApplication.java
@@ -0,0 +1,13 @@
+package com.introduce.seoulgil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulgilApplication.class, args);
+	}
+
+}
