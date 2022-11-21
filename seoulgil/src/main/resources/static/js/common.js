0fd120638af6035f2b645c9f0e96fe368d0c4a16
khlt <kh356113@gmail.com>
2022-11-21 14:50:32 +0900

    이용후기2

--- seoulgil/src/main/resources/static/js/common.js
+++ seoulgil/src/main/resources/static/js/common.js
@@ -1,18 +1,18 @@
 // 헤더
 
-let header = document.querySelector(".mainList");
-let headerHeight = header.offsetHeight;
+let header1 = document.querySelector(".mainList");
+let headerHeight = header1.offsetHeight;
 
 window.onscroll = function () {
     let windowTop = window.scrollY;
     // 스크롤 세로값이 헤더높이보다 크거나 같으면
     // 헤더에 클래스 'drop'을 추가한다
     if (windowTop >= headerHeight * 6) {
-        header.classList.add("drop");
+        header1.classList.add("drop");
     }
     // 아니면 클래스 'drop'을 제거
     else {
-        header.classList.remove("drop");
+        header1.classList.remove("drop");
     }
 };
 //로그인
