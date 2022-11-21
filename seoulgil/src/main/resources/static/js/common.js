53d3bd6a1221cf235963ffb15b41ab1bd044e6c0
khlt <kh356113@gmail.com>
2022-11-21 20:56:21 +0900

    드랍다운헤더

--- seoulgil/src/main/resources/static/js/common.js
+++ seoulgil/src/main/resources/static/js/common.js
@@ -1,13 +1,14 @@
 // 헤더
 
-let header1 = document.querySelector(".mainList");
+let header1 = document.querySelector(".nav-container");
 let headerHeight = header1.offsetHeight;
 
 window.onscroll = function () {
     let windowTop = window.scrollY;
     // 스크롤 세로값이 헤더높이보다 크거나 같으면
     // 헤더에 클래스 'drop'을 추가한다
-    if (windowTop >= headerHeight * 6) {
+    console.log("scrollY");
+    if (windowTop >= headerHeight) {
         header1.classList.add("drop");
     }
     // 아니면 클래스 'drop'을 제거
